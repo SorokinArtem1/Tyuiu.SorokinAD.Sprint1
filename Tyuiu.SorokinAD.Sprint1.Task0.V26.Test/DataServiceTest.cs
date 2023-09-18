aeee500f35b86fbbe604f614f7e99f937848a978
Tyuiu.SorokinAD@outlook.com <abuzius@mail.ru>
2023-09-18 17:12:43 +0500

    Add project files.

---
+++ Tyuiu.SorokinAD.Sprint1.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SorokinAD.Sprint1.Task0.V26.Lib;
+
+namespace Tyuiu.SorokinAD.Sprint1.Task0.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ Tyuiu.SorokinAD.Sprint1.Task0.V26/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SorokinAD.Sprint1.Task0.V26.Lib;
+
+namespace Tyuiu.SorokinAD.Sprint1.Task0.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1| Выполнил: Сорокин А. Д. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #26                                                              *");
+            Console.WriteLine("* Выполнил Сорокин А. Д. | ИИПб-23-2                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать Программу, которая вычисляет выражение 4 * 5 / 2 - 18 / 2 / 3  *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 4 * 5 / 2 - 18 / 2 / 3                                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Резльтат                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+        }
+    }
+}
